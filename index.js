1600d2563532ed9f4d8c02fe9cb56ba0886177c2
braxtonwright <33258249+braxtonwright@users.noreply.github.com>
2023-12-14 16:59:31 -0700

    Replace all template information with some of my info
    The only thing I didn't add is my resume because it is a WIP and my personal email address.
    This also, reorganized the file structor so that the files are easier to find and I also removed some unnessary files not used for the page.

--- index.js
+++ index.js
@@ -27,9 +27,7 @@ let isBackToTopRendered = false;
 let alterStyles = (isBackToTopRendered) => {
   backToTopButton.style.visibility = isBackToTopRendered ? "visible" : "hidden";
   backToTopButton.style.opacity = isBackToTopRendered ? 1 : 0;
-  backToTopButton.style.transform = isBackToTopRendered
-    ? "scale(1)"
-    : "scale(0)";
+  backToTopButton.style.transform = isBackToTopRendered ? "scale(1)" : "scale(0)";
 };
 
 window.addEventListener("scroll", () => {
@@ -40,4 +38,12 @@ window.addEventListener("scroll", () => {
     isBackToTopRendered = false;
     alterStyles(isBackToTopRendered);
   }
+});
+
+// Apply a different style to the below images if they fail to load their images (Added by Braxton)
+// Grab all the elements that have the class name of "contrastImage" and add a error event listener for each one so it can check to see if the image loaded correctly.  If it has not, clear the class name from said element, so it is rendered like the rest of the page https://stackoverflow.com/a/37941811 and https://www.geeksforgeeks.org/how-to-check-whether-an-image-is-loaded-or-not/
+[...document.getElementsByClassName("contrastImage")].forEach(element => {
+    element.addEventListener('error', event => {
+        element.className = "";
+    })
 });
